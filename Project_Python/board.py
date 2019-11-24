e51ef6c575a472b61b97834639308db0a05b3e2f
Mihnea <savum@thehutgroup.com>
2019-11-24 14:46:40 +0000

    Log board state

--- Project_Python/board.py
+++ Project_Python/board.py
@@ -9,7 +9,11 @@ class Board:
         positions = board_state.split(',')
         self.north = positions[:8]
         self.south = positions[8:]
+        self.print_board()
 
     def print_board(self):
-        self.log_file.write(str(self.north[7]) + " -- " + str(self.north[:7:-1]))
-        self.log_file.write(str(self.south[:7]) + " -- " + str(self.south[7]))
+        reversed_north = self.north[:7]
+        reversed_north.reverse()
+
+        self.log_file.write(str(self.north[7]) + " -- " + str(reversed_north) + '\n')
+        self.log_file.write(str(self.south[:7]) + " -- " + str(self.south[7]) + '\n\n')
