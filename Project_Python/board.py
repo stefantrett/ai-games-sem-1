a8a67b8ae166b1c83e965edc7a8aae76d847c6d6
Mihnea <savum@thehutgroup.com>
2019-11-24 15:21:18 +0000

    Changed board positions - home/base thingy is now on position 0 for both - try to not move randomly if illegal

--- Project_Python/board.py
+++ Project_Python/board.py
@@ -1,19 +1,23 @@
 class Board:
 
     def __init__(self):
-        self.north = [7, 7, 7, 7, 7, 7, 7, 0]
-        self.south = [7, 7, 7, 7, 7, 7, 7, 0]
+        self.north = [0, 7, 7, 7, 7, 7, 7, 7]
+        self.south = [0, 7, 7, 7, 7, 7, 7, 7]
         self.log_file = open("board_log.txt", "w")
 
     def update(self, board_state):
         positions = board_state.split(',')
-        self.north = positions[:8]
-        self.south = positions[8:]
+
+        self.north[0] = positions[7]
+        self.north[1:8] = positions[:7]
+
+        self.south[0] = positions[15]
+        self.south[1:8] = positions[8:15]
         self.print_board()
 
     def print_board(self):
-        reversed_north = self.north[:7]
+        reversed_north = self.north[1:]
         reversed_north.reverse()
 
-        self.log_file.write(str(self.north[7]) + " -- " + str(reversed_north) + '\n')
-        self.log_file.write(str(self.south[:7]) + " -- " + str(self.south[7]) + '\n\n')
+        self.log_file.write(str(self.north[0]) + " -- " + str(reversed_north) + '\n')
+        self.log_file.write(str(self.south[1:]) + " -- " + str(self.south[0]) + '\n\n')
