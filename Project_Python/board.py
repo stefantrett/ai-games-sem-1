af45ca0d1d1b68e122364fcf2af38987d3b935f2
Stefan Michael Trett <stefantrett@gmail.com>
2019-11-24 15:15:17 +0000

    Util class - contains log method

--- Project_Python/board.py
+++ Project_Python/board.py
@@ -1,9 +1,11 @@
+from util import log
+
+
 class Board:
 
     def __init__(self):
         self.north = [7, 7, 7, 7, 7, 7, 7, 0]
         self.south = [7, 7, 7, 7, 7, 7, 7, 0]
-        self.log_file = open("board_log.txt", "w")
 
     def update(self, board_state):
         positions = board_state.split(',')
@@ -15,5 +17,5 @@ class Board:
         reversed_north = self.north[:7]
         reversed_north.reverse()
 
-        self.log_file.write(str(self.north[7]) + " -- " + str(reversed_north) + '\n')
-        self.log_file.write(str(self.south[:7]) + " -- " + str(self.south[7]) + '\n\n')
+        log(str(self.north[7]) + " -- " + str(reversed_north) + '\n')
+        log(str(self.south[:7]) + " -- " + str(self.south[7]) + '\n\n')
---
+++ Project_Python/util.py
@@ -0,0 +1,5 @@
+log_file = open("log.txt", "w")
+
+
+def log(some_string):
+    log_file.write(some_string)
